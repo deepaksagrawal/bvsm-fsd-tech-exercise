85ebfc4c8b14220c51897afcd64aeb29ca3b69ef
deepaksagrawal <s.deepak.agrawal@gmail.com>
2017-07-17 13:17:17 +0530

    Product list

--- src/App.js
+++ src/App.js
@@ -3,19 +3,28 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+  constructor() {
+        super();
+        this.state = { items: [] };
+    }
+    
+    componentDidMount() {
+        fetch(`http://localhost:4000/products`) 
+            .then(result=> {
+                this.setState({items:result.json()});
+            });
+              alert(this.state.items);
+    }
+    
+    render() {        
+        return(
+            <div>
+                <div>Items:</div>
+                { this.state.items }          
+            </div>  
+        );
+    } 
 }
 
 export default App;
